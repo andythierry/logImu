b0b9b751e06564f49eaebe2be0a053770c96fe08
andythierry <andythierry@me.com>
2022-04-26 20:18:18 +0400

    arr fix

--- line.js
+++ line.js
@@ -17,7 +17,7 @@ var myChart2 = new Chart(document.getElementById("line-chart"), {
     labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
     datasets: [{
       data: [86,114,106,106,107,111,133,221,783,2478],
-      label: arr[0][0],
+      label: arr[0],
       borderColor: "#3e95cd",
       fill: false
     }, {
